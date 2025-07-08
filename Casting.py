ef0f7c581361a3024733725f5afbc79c041cba76
Brawlstars902 <aparna.rautela@gmail.com>
2025-07-08 07:05:29 +0530

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,10 @@
+y=1
+z=3.141592653589723213
+print('The data types before type casting')
+print(y,type(y))
+print(z,type(z))
+y=float(y)
+z=int(z)
+print('The data types after type casting')
+print(y,type(y))
+print(z,type(z))
